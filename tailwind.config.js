c8fa77637fb1777fc690a2fdfbeda72eb7c022cf
Görkem <123131846+MrGorkemli@users.noreply.github.com>
2023-10-24 16:42:28 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {},
+    container:true,
   },
   plugins: [],
 }
