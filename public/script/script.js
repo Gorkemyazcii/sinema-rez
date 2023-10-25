901659c8a275af474c201001ee545f31cebe01ce
Görkem <123131846+MrGorkemli@users.noreply.github.com>
2023-10-25 14:45:40 +0300

    Update script.js

--- public/script/script.js
+++ public/script/script.js
@@ -2,6 +2,7 @@ const container = document.querySelector('.container');
 const count = document.getElementById('count');
 const amount = document.getElementById('amount');
 const select = document.getElementById('movie');
+const seats = document.querySelectorAll('.seat:not(.reserved)')
 
 
 container.addEventListener('click', function(e){
@@ -20,9 +21,41 @@ select.addEventListener('change', function(e) {
 })
 
 function calculateTotal(){
-    let selectedSeatCount = container.querySelectorAll('.seat.selected').length;
+    const selectedSeats = container.querySelectorAll('.seat.selected');
+
+    const selecetedSeatsArr = [];
+    const seatsArr = [];
+
+    selectedSeats.forEach(function(seat){
+        selecetedSeatsArr.push(seat);
+    });
+
+    seats.forEach(function(seat){
+        seatsArr.push(seat);
+    });
+
+    let selectedSeatIndexs = selecetedSeatsArr.map(function(seat){
+        return seatsArr.indexOf(seat);
+    });
+
+    console.log(selectedSeatIndexs);
+
+
+
+    let selectedSeatCount = selectedSeats.length;
     //console.log(selectedSeatCount);
+    //console.log(selectedSeats);
+    //console.log(seats);
+
     let price = select.value;
     count.innerText=selectedSeatCount;
     amount.innerText = selectedSeatCount *price;
+
+    saveToLocalStorage(selectedSeatIndexs);
+
+}
+
+function saveToLocalStorage(indexs){
+    localStorage.setItem('selectedSeats',JSON.stringify(indexs));
+    localStorage.setItem('selectedMovieIndex',select.selectedIndex);
 }
